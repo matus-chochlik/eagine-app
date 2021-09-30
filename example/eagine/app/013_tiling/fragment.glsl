e665527df724e349e8302adfd0a6a0f6099f09ba
Matus Chochlik <chochlik@gmail.com>
2021-09-30 07:32:37 +0200

    Addtions to example + added new tiling texture build instructions

--- example/eagine/app/013_tiling/fragment.glsl
+++ example/eagine/app/013_tiling/fragment.glsl
@@ -2,6 +2,7 @@
 in vec3 vertColor;
 in vec3 vertTilingCoord;
 in vec2 vertTilesetCoord;
+in vec2 vertGridCoord;
 out vec3 fragColor;
 uniform isampler2DArray TilingTex;
 uniform sampler2DArray TilesetTex;
@@ -10,5 +11,7 @@ void main() {
 	vec3 tileCoord = vec3(
 		vertTilesetCoord,
 		texture(TilingTex, vertTilingCoord).r);
-    fragColor = vertColor * texture(TilesetTex, tileCoord).r * 2.0;
+	vec2 gridCoord = abs(vertGridCoord - round(vertGridCoord));
+	float grid = 2.0 - exp(-min(gridCoord.x, gridCoord.y)*64.0) * 0.5;
+    fragColor = vertColor * texture(TilesetTex, tileCoord).r * grid;
 }
