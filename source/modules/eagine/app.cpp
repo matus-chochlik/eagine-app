5cf9c16d7202fc926aad07ba427e0ce7d6d18615
Matus Chochlik <chochlik@gmail.com>
2022-08-19 17:27:05 +0200

    Added modularized source code

--- source/modules/eagine/app.cpp
+++ source/modules/eagine/app.cpp
@@ -15,4 +15,5 @@ export import :interface;
 export import :implementation;
 export import :framedump_raw;
 export import :context;
+export import :geometry;
 export import :camera;
---
+++ source/modules/eagine/geometry.cpp
@@ -0,0 +1,77 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+export module eagine.app:geometry;
+
+import eagine.core.types;
+import eagine.core.memory;
+import eagine.shapes;
+import eagine.oglplus;
+import :context;
+
+namespace eagine::app {
+//------------------------------------------------------------------------------
+export class geometry_and_bindings : public oglplus::geometry_and_bindings {
+    using base = oglplus::geometry_and_bindings;
+
+public:
+    using base::init;
+
+    auto init(
+      const oglplus::shape_generator& shape,
+      video_context& vc,
+      memory::buffer& temp) -> auto& {
+        const auto& glapi = vc.gl_api();
+        oglplus::geometry_and_bindings::init(glapi, shape, temp);
+        return *this;
+    }
+
+    auto init(const oglplus::shape_generator& shape, video_context& vc)
+      -> auto& {
+        return init(shape, vc, vc.parent().buffer());
+    }
+
+    auto init(
+      const std::shared_ptr<shapes::generator>& gen,
+      video_context& vc,
+      memory::buffer& temp) -> auto& {
+        const auto& glapi = vc.gl_api();
+        oglplus::shape_generator shape(glapi, gen);
+        return init(shape, vc, temp);
+    }
+
+    auto init(const std::shared_ptr<shapes::generator>& gen, video_context& vc)
+      -> auto& {
+        return init(gen, vc, vc.parent().buffer());
+    }
+
+    auto clean_up(video_context& vc) -> auto& {
+        base::clean_up(vc.gl_api());
+        return *this;
+    }
+
+    using base::use;
+
+    auto use(video_context& vc) -> auto& {
+        base::use(vc.gl_api());
+        return *this;
+    }
+
+    using base::draw;
+
+    auto draw(video_context& vc) -> auto& {
+        base::draw(vc.gl_api());
+        return *this;
+    }
+
+    auto use_and_draw(video_context& vc) -> auto& {
+        return use(vc).draw(vc);
+    }
+};
+//------------------------------------------------------------------------------
+} // namespace eagine::app
+
