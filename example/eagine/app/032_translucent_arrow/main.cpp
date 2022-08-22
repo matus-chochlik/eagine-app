11bf8e910f320110e4a9eb1289ee5b680b2641af
Matus Chochlik <chochlik@gmail.com>
2022-08-22 11:56:47 +0200

    Modulerized some examples

--- example/eagine/app/032_translucent_arrow/main.cpp
+++ example/eagine/app/032_translucent_arrow/main.cpp
@@ -6,6 +6,12 @@
 ///  http://www.boost.org/LICENSE_1_0.txt
 ///
 
+#if EAGINE_APP_MODULE
+import eagine.core;
+import eagine.shapes;
+import eagine.oglplus;
+import eagine.app;
+#else
 #include <eagine/oglplus/gl.hpp>
 #include <eagine/oglplus/gl_api.hpp>
 
@@ -21,6 +27,7 @@
 #include <eagine/shapes/value_tree.hpp>
 #include <eagine/timeout.hpp>
 #include <eagine/value_tree/json.hpp>
+#endif
 
 #include "resources.hpp"
 
@@ -207,4 +214,13 @@ auto establish(main_ctx&) -> std::unique_ptr<launchpad> {
     return {std::make_unique<example_launchpad>()};
 }
 //------------------------------------------------------------------------------
+auto example_main(main_ctx& ctx) -> int {
+    return default_main(ctx, establish(ctx));
+}
 } // namespace eagine::app
+
+#if EAGINE_APP_MODULE
+auto main(int argc, const char** argv) -> int {
+    return eagine::default_main(argc, argv, eagine::app::example_main);
+}
+#endif
