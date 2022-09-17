bbc28e03275b22ac9e6fcd44764c7a5b1254eb69
Matus Chochlik <chochlik@gmail.com>
2022-09-17 13:39:15 +0200

    Additions and changes in resource_loader

--- source/modules/eagine/resource_transfer.cpp
+++ source/modules/eagine/resource_transfer.cpp
@@ -13,12 +13,37 @@ import eagine.core.utility;
 import eagine.core.runtime;
 import eagine.shapes;
 import eagine.oglplus;
+import eagine.msgbus;
 import <utility>;
 
 namespace eagine::app {
 //------------------------------------------------------------------------------
+export class resource_request_result {
+public:
+    resource_request_result(identifier_t req_id, url loc) noexcept
+      : _request_id{req_id}
+      , _locator(std::move(loc)) {}
+
+    explicit operator bool() const noexcept {
+        return _request_id != 0;
+    }
+
+    auto request_id() const noexcept -> identifier_t {
+        return _request_id;
+    }
+
+    auto locator() const noexcept -> const url& {
+        return _locator;
+    }
+
+private:
+    identifier_t _request_id;
+    url _locator;
+};
+//------------------------------------------------------------------------------
 export struct resource_loader_signals {
-    signal<void(identifier_t, const shapes::generator&) noexcept> shape_loaded;
+    signal<void(identifier_t, const std::shared_ptr<shapes::generator>&) noexcept>
+      shape_loaded;
 
     signal<void(identifier_t, oglplus::buffer_name) noexcept> buffer_loaded;
 
@@ -32,22 +57,27 @@ export struct resource_loader_signals {
     signal<void(identifier_t, oglplus::program_name) noexcept> program_loaded;
 };
 //------------------------------------------------------------------------------
-export class resource_loader : public resource_loader_signals {
+export class resource_loader
+  : public msgbus::resource_data_consumer_node
+  , public resource_loader_signals {
+    using base = msgbus::resource_data_consumer_node;
+
 public:
-    auto request_gl_buffer(const url& locator) noexcept
-      -> std::pair<identifier_t, const url&>;
+    auto update() noexcept -> work_done;
 
-    auto request_gl_texture(const url& locator) noexcept
-      -> std::pair<identifier_t, const url&>;
+    auto request_shape_generator(url locator) noexcept
+      -> resource_request_result;
 
-    auto request_gl_shader_source(const url& locator) noexcept
-      -> std::pair<identifier_t, const url&>;
+    auto request_gl_buffer(url locator) noexcept -> resource_request_result;
 
-    auto request_gl_shader(const url& locator) noexcept
-      -> std::pair<identifier_t, const url&>;
+    auto request_gl_texture(url locator) noexcept -> resource_request_result;
 
-    auto request_gl_program(const url& locator) noexcept
-      -> std::pair<identifier_t, const url&>;
+    auto request_gl_shader_source(url locator) noexcept
+      -> resource_request_result;
+
+    auto request_gl_shader(url locator) noexcept -> resource_request_result;
+
+    auto request_gl_program(url locator) noexcept -> resource_request_result;
 
 private:
 };
--- source/modules/eagine/resource_transfer_impl.cpp
+++ source/modules/eagine/resource_transfer_impl.cpp
@@ -9,33 +9,60 @@ module eagine.app;
 
 import eagine.core.types;
 import eagine.core.memory;
+import eagine.core.reflection;
+import eagine.core.utility;
 import eagine.core.runtime;
+import eagine.shapes;
+import eagine.oglplus;
 
 namespace eagine::app {
 //------------------------------------------------------------------------------
-auto resource_loader::request_gl_buffer(const url& locator) noexcept
-  -> std::pair<identifier_t, const url&> {
-    return {0, locator};
+auto resource_loader::request_shape_generator(url locator) noexcept
+  -> resource_request_result {
+    const auto args{locator.query()};
+    shapes::vertex_attrib_kinds attrs;
+    for(const auto& info : enumerator_mapping(
+          std::type_identity<shapes::vertex_attrib_kind>{}, default_selector)) {
+        if(const auto pos{args.find(info.name)}; pos != args.end()) {
+        }
+    }
+
+    std::shared_ptr<shapes::generator> gen;
+    if(locator.has_path("/cube")) {
+        gen = shapes::unit_cube(attrs);
+    }
+    return {0, std::move(locator)};
 }
 //------------------------------------------------------------------------------
-auto resource_loader::request_gl_texture(const url& locator) noexcept
-  -> std::pair<identifier_t, const url&> {
-    return {0, locator};
+auto resource_loader::request_gl_buffer(url locator) noexcept
+  -> resource_request_result {
+    return {0, std::move(locator)};
 }
 //------------------------------------------------------------------------------
-auto resource_loader::request_gl_shader_source(const url& locator) noexcept
-  -> std::pair<identifier_t, const url&> {
-    return {0, locator};
+auto resource_loader::request_gl_texture(url locator) noexcept
+  -> resource_request_result {
+    return {0, std::move(locator)};
 }
 //------------------------------------------------------------------------------
-auto resource_loader::request_gl_shader(const url& locator) noexcept
-  -> std::pair<identifier_t, const url&> {
-    return {0, locator};
+auto resource_loader::request_gl_shader_source(url locator) noexcept
+  -> resource_request_result {
+    return {0, std::move(locator)};
 }
 //------------------------------------------------------------------------------
-auto resource_loader::request_gl_program(const url& locator) noexcept
-  -> std::pair<identifier_t, const url&> {
-    return {0, locator};
+auto resource_loader::request_gl_shader(url locator) noexcept
+  -> resource_request_result {
+    return {0, std::move(locator)};
+}
+//------------------------------------------------------------------------------
+auto resource_loader::request_gl_program(url locator) noexcept
+  -> resource_request_result {
+    return {0, std::move(locator)};
+}
+//------------------------------------------------------------------------------
+auto resource_loader::update() noexcept -> work_done {
+    some_true something_done{base::update()};
+
+    return something_done;
 }
 //------------------------------------------------------------------------------
 } // namespace eagine::app
