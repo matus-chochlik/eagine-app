aea011a81b222b2d332d56a759d0274ae71ca6cc
Matus Chochlik <chochlik@gmail.com>
2022-10-14 10:10:23 +0200

    Additions to loaded_resource + updated oglplus sub-module

--- source/modules/eagine/loaded_resource.cpp
+++ source/modules/eagine/loaded_resource.cpp
@@ -13,6 +13,7 @@ import eagine.core.runtime;
 import eagine.shapes;
 import eagine.oglplus;
 import :context;
+import :geometry;
 import :resource_loader;
 
 namespace eagine::app {
@@ -69,7 +70,7 @@ public:
           this, loader.gl_program_loaded);
     }
 
-    /// @brief Clean's up the resource.
+    /// @brief Clean's up this resource.
     void clean_up(video_context& video, resource_loader& loader) {
         video.gl_api().clean_up(std::move(_res()));
         if(_sig_key) {
@@ -112,6 +113,79 @@ private:
             if(is_loaded()) {
                 _request_id = 0;
                 this->loaded(prog, input_bindings);
+            }
+        }
+    }
+
+    signal_binding_key _sig_key{};
+};
+//------------------------------------------------------------------------------
+export template <>
+class loaded_resource<geometry_and_bindings>
+  : public geometry_and_bindings
+  , public loaded_resource_base {
+    using _res_t = geometry_and_bindings;
+
+    auto _res() noexcept -> _res_t& {
+        return *this;
+    }
+
+public:
+    /// @brief Signal emmitted when the resource is successfully loaded.
+    signal<void(const geometry_and_bindings& geom) noexcept> loaded;
+
+    /// @brief Constructor specifying the resource locator.
+    loaded_resource(url locator) noexcept
+      : loaded_resource_base{std::move(locator)} {}
+
+    /// @brief Delay-initializes the resource.
+    void init(video_context&, resource_loader& loader) {
+        _sig_key =
+          connect<&loaded_resource::_handle_gl_geometry_and_bindings_loaded>(
+            this, loader.gl_geometry_and_bindings_loaded);
+    }
+
+    /// @brief Clean's up this resource.
+    void clean_up(video_context&, resource_loader& loader) {
+        if(_sig_key) {
+            loader.gl_geometry_and_bindings_loaded.disconnect(_sig_key);
+            _sig_key = {};
+        }
+    }
+
+    /// @brief Constructor specifying the locator and initializing the resource.
+    loaded_resource(url locator, video_context& video, resource_loader& loader)
+      : loaded_resource{std::move(locator)} {
+        init(video, loader);
+    }
+
+    /// @brief Indicates if this resource is loaded.
+    auto is_loaded() const noexcept -> bool {
+        return this->is_initialized();
+    }
+
+    /// @brief Updates the resource, possibly doing resource load request.
+    auto update(video_context& video, resource_loader& loader) -> work_done {
+        if(!is_loaded() && !is_loading()) {
+            if(const auto request{
+                 loader.request_gl_geometry_and_bindings(_locator, video)}) {
+                _request_id = request.request_id();
+                return true;
+            }
+        }
+        return false;
+    }
+
+private:
+    void _handle_gl_geometry_and_bindings_loaded(
+      const identifier_t request_id,
+      std::reference_wrapper<geometry_and_bindings> ref,
+      const url&) noexcept {
+        if(request_id == _request_id) {
+            _res() = std::move(ref.get());
+            if(is_loaded()) {
+                _request_id = 0;
+                // this->loaded(_res());
             }
         }
     }
