966164119ab75dd215b677cdd080b3bbc02eba3f
Matus Chochlik <chochlik@gmail.com>
2023-03-08 09:03:26 +0100

    Adaptations to changes in msgbus

--- source/modules/eagine/context_impl.cpp
+++ source/modules/eagine/context_impl.cpp
@@ -552,7 +552,7 @@ void execution_context::clean_up() noexcept {
 //------------------------------------------------------------------------------
 void execution_context::update() noexcept {
     const auto exec_time{measure_time_interval("appUpdate")};
-    _registry.update_all();
+    _registry.update_and_process();
     _loader.update();
     extract(_state).update_activity();
     for(auto& provider : _hmi_providers) {
