ec18bafb220fa0993820311cfb74035e1caf15b8
Matus Chochlik <chochlik@gmail.com>
2023-05-14 07:33:16 +0200

    Small changes in the replace_tiling helper script

--- assets/tilings/_replace_tiling.sh
+++ assets/tilings/_replace_tiling.sh
@@ -3,11 +3,15 @@ function find_dest() {
 	find "$(dirname ${0})" \
 		-type f \
 		-name '*.txt' \
-		-size $(stat -c %s "${1:-/dev/zero}")c \
+		-size ${2}c \
 		-exec grep -H -c -e '\([A-F0-9]\)\1' {} \; |\
 	grep -v ':0$' |\
 	cut -d: -f1 |\
 	head -n 1
 }
 
-mv "${1}" "$(find_dest ${1})"
+for f
+do
+	size=$(stat -c %s "${f}") || exit 1
+	mv "${f}" "$(find_dest ${f} ${size})" && echo "${f}"
+done
