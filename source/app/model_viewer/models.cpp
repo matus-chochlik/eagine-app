b361b0476448c0bbe37b68d70dfafb561111917f
Matus Chochlik <chochlik@gmail.com>
2023-08-28 07:51:37 +0200

    Small changes in model_viewer

--- source/app/model_viewer/models.cpp
+++ source/app/model_viewer/models.cpp
@@ -16,6 +16,28 @@ model_viewer_models::model_viewer_models(
     load("Traffic cone", url{"json:///TraficCone"}, ctx, video);
     load("Guitar", url{"json:///Guitar"}, ctx, video);
     load("Stool", url{"json:///Stool"}, ctx, video);
+    load(
+      "Twisted torus",
+      url{"shape:///unit_twisted_torus?position=true+normal=true"},
+      ctx,
+      video);
+    load(
+      "Torus",
+      url{"shape:///unit_torus"
+          "?position=true+normal=true+tangent=true+bitangent=true"},
+      ctx,
+      video);
+    load(
+      "Round cube",
+      url{"shape:///unit_round_cube"
+          "?position=true+normal=true+tangent=true+bitangent=true"},
+      ctx,
+      video);
+    load(
+      "Icosahedron",
+      url{"shape:///unit_icosahedron?position=true+normal=true"},
+      ctx,
+      video);
 }
 //------------------------------------------------------------------------------
 auto model_viewer_models::bounding_sphere() noexcept -> oglplus::sphere {
