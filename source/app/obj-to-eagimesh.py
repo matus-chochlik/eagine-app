d2123e7fd966652f4f5565eaefea876d2b39c2c6
Matus Chochlik <chochlik@gmail.com>
2023-09-19 04:02:26 +0200

    Moved the obj-to-eagimesh tool to source/app

--- source/app/obj-to-eagimesh.py
+++ source/app/obj-to-eagimesh.py
@@ -4,8 +4,6 @@
 # Distributed under the Boost Software License, Version 1.0.
 # See accompanying file LICENSE_1_0.txt or copy at
 #  http://www.boost.org/LICENSE_1_0.txt
-#
-# usage: blender -b myfile.blend -P export_mesh.py [-- [--help|options...]]
 
 import os
 import re
