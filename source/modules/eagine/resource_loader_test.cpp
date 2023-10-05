1e9d03ec9212eda9cad5837490b5e36323a30f02
Matus Chochlik <chochlik@gmail.com>
2023-10-05 08:35:10 +0200

    Added the initial version of unit testing framework + updated sub-module

---
+++ source/modules/eagine/resource_loader_test.cpp
@@ -0,0 +1,39 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+
+#include <eagine/testing/unit_begin_app.hpp>
+import eagine.core;
+//------------------------------------------------------------------------------
+// plain text
+//------------------------------------------------------------------------------
+struct test_request_plain_text : eagitest::app_case {
+    using launcher = eagitest::launcher<test_request_plain_text>;
+
+    test_request_plain_text(auto& s, auto& ec)
+      : eagitest::app_case{s, ec, 1, "plain text"} {}
+
+    auto is_done() noexcept -> bool final {
+        return true;
+    }
+
+    void update() noexcept final {}
+};
+//------------------------------------------------------------------------------
+// main
+//------------------------------------------------------------------------------
+auto test_main(eagine::test_ctx& ctx) -> int {
+    eagitest::app_suite test{ctx, "resource loader", 1};
+    test.once<test_request_plain_text>();
+    return test.exit_code();
+}
+//------------------------------------------------------------------------------
+auto main(int argc, const char** argv) -> int {
+    return eagine::test_main_impl(argc, argv, test_main);
+}
+//------------------------------------------------------------------------------
+#include <eagine/testing/unit_end_app.hpp>
