e6912b4192c394e8cc1ee42691892b8da0183549
Matus Chochlik <chochlik@gmail.com>
2023-10-10 07:38:47 +0200

    Fixes in resource loader and added new unit test

--- source/modules/eagine/resource_loader_test.cpp
+++ source/modules/eagine/resource_loader_test.cpp
@@ -62,11 +62,72 @@ struct test_request_plain_text : eagitest::app_case {
     bool content_is_ok{false};
 };
 //------------------------------------------------------------------------------
+// float vector
+//------------------------------------------------------------------------------
+struct test_request_float_vector : eagitest::app_case {
+    using launcher = eagitest::launcher<test_request_float_vector>;
+
+    test_request_float_vector(auto& s, auto& ec)
+      : eagitest::app_case{s, ec, 2, "float vector"}
+      , ints{eagine::url{"json:///TestInts"}, ec} {
+        ints.loaded.connect(
+          make_callable_ref<&test_request_float_vector::on_loaded>(this));
+        too_long.reset();
+    }
+
+    void on_loaded(
+      const eagine::app::float_vector_resource::load_info& info) noexcept {
+        load_signal_received = true;
+        locator_is_ok = info.base.locator.has_scheme("json") and
+                        info.base.locator.has_path("/TestInts");
+        if(info.base.values.size() == 7 and ints.size() == 7) {
+            content_is_ok = true;
+            content_is_ok = content_is_ok and int(info.base.values[0]) == 1;
+            content_is_ok = content_is_ok and int(ints[1]) == 2;
+            content_is_ok = content_is_ok and int(ints[2]) == 3;
+            content_is_ok = content_is_ok and int(ints[3]) == 5;
+            content_is_ok = content_is_ok and int(ints[4]) == 8;
+            content_is_ok = content_is_ok and int(ints[5]) == 13;
+            content_is_ok = content_is_ok and int(info.base.values[6]) == 21;
+        }
+    }
+
+    auto is_loaded() const noexcept {
+        return load_signal_received and locator_is_ok and content_is_ok;
+    }
+
+    auto is_done() noexcept -> bool final {
+        return too_long or is_loaded();
+    }
+
+    void update() noexcept final {
+        if(not ints) {
+            ints.load_if_needed(context());
+        }
+    }
+
+    void clean_up() noexcept final {
+        check(ints.is_loaded(), "values are loaded");
+        check(load_signal_received, "load signal received");
+        check(locator_is_ok, "locator is ok");
+        check(content_is_ok, "content is ok");
+
+        ints.clean_up(context());
+    }
+
+    eagine::timeout too_long{std::chrono::seconds{10}};
+    eagine::app::float_vector_resource ints;
+    bool load_signal_received{false};
+    bool locator_is_ok{false};
+    bool content_is_ok{false};
+};
+//------------------------------------------------------------------------------
 // main
 //------------------------------------------------------------------------------
 auto test_main(eagine::test_ctx& ctx) -> int {
-    eagitest::app_suite test{ctx, "resource loader", 1};
+    eagitest::app_suite test{ctx, "resource loader", 2};
     test.once<test_request_plain_text>();
+    test.once<test_request_float_vector>();
     return test.exit_code();
 }
 //------------------------------------------------------------------------------
