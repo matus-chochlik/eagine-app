9168cc1e12c2f5906249f4660a240de82096dabe
Matus Chochlik <chochlik@gmail.com>
2023-10-22 11:31:54 +0200

    Refactoring in resource provider

--- source/app/resource_provider/main.cpp
+++ source/app/resource_provider/main.cpp
@@ -15,78 +15,110 @@ import std;
 
 namespace eagine {
 //------------------------------------------------------------------------------
+class resource_provider : public main_ctx_object {
+public:
+    resource_provider(main_ctx_parent parent) noexcept;
+
+    auto is_done() noexcept -> bool;
+
+    void update();
+
+    void finish();
+
+private:
+    msgbus::endpoint _consumer_bus{"ResConEndp", as_parent()};
+    msgbus::endpoint _provider_bus{"ResProEndp", as_parent()};
+
+    msgbus::resource_data_consumer_node _resource_consumer{_consumer_bus};
+    app::resource_provider_driver _driver{as_parent(), _resource_consumer};
+    msgbus::resource_data_server_node _resource_server{_provider_bus, _driver};
+
+    std::optional<msgbus::router> _router;
+    std::optional<timeout> _idle_too_long;
+    bool _only_if_busy;
+};
+//------------------------------------------------------------------------------
+resource_provider::resource_provider(main_ctx_parent parent) noexcept
+  : main_ctx_object{"ResrcPrvdr", parent}
+  , _idle_too_long{app_config()
+                     .get<std::chrono::seconds>(
+                       "app.resource_provider.max_idle_time")
+                     .and_then(
+                       [&](auto max_idle_time) -> std::optional<timeout> {
+                           return {{max_idle_time}};
+                       })}
+  , _only_if_busy{app_config().is_set("app.resource_provider.only_if_busy")} {
+
+    auto& ctx{main_context()};
+
+    msgbus::router_address address{ctx};
+    msgbus::connection_setup conn_setup(ctx);
+
+    conn_setup.setup_connectors(_resource_server, address);
+    conn_setup.setup_connectors(_resource_consumer, address);
+
+    if(app_config().is_set("app.resource_provider.with_router")) {
+        log_info("starting with message bus router");
+
+        _router.emplace(ctx);
+        _router->add_ca_certificate_pem(ca_certificate_pem(ctx));
+        _router->add_certificate_pem(msgbus::router_certificate_pem(ctx));
+        msgbus::setup_acceptors(ctx, *_router);
+    }
+}
+//------------------------------------------------------------------------------
+auto resource_provider::is_done() noexcept -> bool {
+    return _resource_server.is_done() or
+           (_idle_too_long and _idle_too_long->is_expired());
+}
+//------------------------------------------------------------------------------
+void resource_provider::update() {
+    _resource_server.update_message_age();
+    some_true something_done{_resource_server.update_and_process_all()};
+    something_done(_resource_consumer.update_and_process_all());
+    if(_router) {
+        something_done(_router->update(8));
+    }
+    if(something_done) {
+        if(not _only_if_busy or _resource_server.has_pending_blobs()) {
+            _idle_too_long->reset();
+        }
+        std::this_thread::yield();
+    } else {
+        std::this_thread::sleep_for(std::chrono::microseconds{1000});
+    }
+}
+//------------------------------------------------------------------------------
+void resource_provider::finish() {
+    if(_router) {
+        _router->finish();
+    }
+}
+//------------------------------------------------------------------------------
+// main
+//------------------------------------------------------------------------------
 auto main(main_ctx& ctx) -> int {
     signal_switch interrupted;
     const auto& log = ctx.log();
 
     enable_message_bus(ctx);
-
     log.info("resource provider starting up");
-
     ctx.system().preinitialize();
 
-    msgbus::router_address address{ctx};
-    msgbus::connection_setup conn_setup(ctx);
-
-    std::optional<msgbus::router> router(ctx);
-    if(ctx.config().is_set("app.resource_provider.with_router")) {
-        log.info("starting with message bus router");
-
-        router.emplace(ctx);
-        router->add_ca_certificate_pem(ca_certificate_pem(ctx));
-        router->add_certificate_pem(msgbus::router_certificate_pem(ctx));
-        msgbus::setup_acceptors(ctx, *router);
-    }
-
-    msgbus::endpoint consumer_bus{main_ctx_object{"ResConEndp", ctx}};
-    msgbus::resource_data_consumer_node resource_consumer{consumer_bus};
-    conn_setup.setup_connectors(resource_consumer, address);
-
-    msgbus::endpoint provider_bus{main_ctx_object{"ResProEndp", ctx}};
-    app::resource_provider_driver driver{ctx, resource_consumer};
-    msgbus::resource_data_server_node resource_provider{provider_bus, driver};
-    conn_setup.setup_connectors(resource_provider, address);
-
-    const bool only_if_busy{
-      ctx.config().is_set("app.resource_provider.only_if_busy")};
-
-    auto idle_too_long{
-      ctx.config()
-        .get<std::chrono::seconds>("app.resource_provider.max_idle_time")
-        .and_then([&](auto max_idle_time) {
-            return std::optional<timeout>{{max_idle_time}};
-        })};
+    resource_provider provider{ctx};
 
     const auto is_done{[&] {
-        return interrupted or resource_provider.is_done() or
-               (idle_too_long and idle_too_long->is_expired());
+        return interrupted or provider.is_done();
     }};
 
     auto& wd = ctx.watchdog();
     wd.declare_initialized();
 
     while(not is_done()) {
-        some_true something_done;
         wd.notify_alive();
-
-        resource_provider.update_message_age();
-        something_done(resource_provider.update_and_process_all());
-        something_done(resource_consumer.update_and_process_all());
-        if(router) {
-            something_done(router->update(8));
-        }
-        if(something_done) {
-            if(not only_if_busy or resource_provider.has_pending_blobs()) {
-                idle_too_long->reset();
-            }
-            std::this_thread::yield();
-        } else {
-            std::this_thread::sleep_for(std::chrono::microseconds{1000});
-        }
+        provider.update();
     }
-    if(router) {
-        router->finish();
-    }
+    provider.finish();
     wd.announce_shutdown();
 
     return 0;
