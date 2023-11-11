9348ad274908fdbfb479c8978c7692198bd40641
Matus Chochlik <chochlik@gmail.com>
2023-11-11 08:34:14 +0100

    Changes in resource_provider

---
+++ source/app/resource_provider/common.cpp
@@ -0,0 +1,40 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+#include "common.hpp"
+#include <cassert>
+
+namespace eagine::app {
+//------------------------------------------------------------------------------
+// simple_string_source_blob_io
+//------------------------------------------------------------------------------
+simple_string_source_blob_io::simple_string_source_blob_io(
+  identifier id,
+  main_ctx_parent parent,
+  std::string content) noexcept
+  : main_ctx_object{id, parent}
+  , _content{std::move(content)} {}
+//------------------------------------------------------------------------------
+auto simple_string_source_blob_io::total_size() noexcept -> span_size_t {
+    return span_size(_content.size());
+}
+//------------------------------------------------------------------------------
+auto simple_string_source_blob_io::fetch_fragment(
+  const span_size_t offs,
+  memory::block dst) noexcept -> span_size_t {
+    return copy(head(view(_content), dst.size()), dst).size();
+}
+//------------------------------------------------------------------------------
+// eagitex_provider_base
+//------------------------------------------------------------------------------
+eagitex_provider_base::eagitex_provider_base(
+  identifier id,
+  main_ctx_parent parent) noexcept
+  : main_ctx_object{id, parent} {}
+//------------------------------------------------------------------------------
+} // namespace eagine::app
+
---
+++ source/app/resource_provider/common.hpp
@@ -0,0 +1,50 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+#ifndef EAGINE_RESOURCE_PROVIDER_COMMON_HPP
+#define EAGINE_RESOURCE_PROVIDER_COMMON_HPP
+
+#include "driver.hpp"
+
+namespace eagine::app {
+//------------------------------------------------------------------------------
+struct simple_string_source_blob_io
+  : main_ctx_object
+  , msgbus::source_blob_io {
+protected:
+    simple_string_source_blob_io(
+      identifier id,
+      main_ctx_parent parent,
+      std::string content) noexcept;
+
+    auto total_size() noexcept -> span_size_t final;
+
+    auto fetch_fragment(const span_size_t offs, memory::block dst) noexcept
+      -> span_size_t final;
+
+private:
+    const std::string _content;
+};
+//------------------------------------------------------------------------------
+struct eagitex_provider_base
+  : main_ctx_object
+  , resource_provider_interface {
+protected:
+    eagitex_provider_base(identifier id, main_ctx_parent parent) noexcept;
+
+    static auto valid_dimension(int d) noexcept -> bool {
+        return (d > 0) and (d <= 64 * 1024);
+    }
+
+    static auto valid_level(int l) noexcept -> bool {
+        return (l >= 0);
+    }
+};
+//------------------------------------------------------------------------------
+} // namespace eagine::app
+
+#endif
