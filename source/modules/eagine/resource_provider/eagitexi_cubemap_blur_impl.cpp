613c1a60c3ed1c7a72ed875849587ed13cb27a2c
Matus Chochlik <chochlik@gmail.com>
2024-03-08 17:34:25 +0100

    Small changes in eagitexi_cubemap_blur_renderer

--- source/modules/eagine/resource_provider/eagitexi_cubemap_blur_impl.cpp
+++ source/modules/eagine/resource_provider/eagitexi_cubemap_blur_impl.cpp
@@ -96,44 +96,50 @@ uniform samplerCube cubeMap;
 uniform int faceIdx;
 uniform int cubeSide;
 uniform int sharpness;
-const mat3[6] cubeFaces = mat3[6](
-  mat3( 0.0, 0.0,-1.0, 0.0,-1.0, 0.0, 1.0, 0.0, 0.0),
-  mat3( 0.0, 0.0, 1.0, 0.0,-1.0, 0.0,-1.0, 0.0, 0.0),
-  mat3( 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0),
-  mat3( 1.0, 0.0, 0.0, 0.0, 0.0,-1.0, 0.0,-1.0, 0.0),
-  mat3( 1.0, 0.0, 0.0, 0.0,-1.0, 0.0, 0.0, 0.0, 1.0),
-  mat3(-1.0, 0.0, 0.0, 0.0,-1.0, 0.0, 0.0, 0.0,-1.0));
+
+mat3 getCubeFace(int f) {
+	return mat3[6](
+		mat3( 0.0, 0.0,-1.0, 0.0,-1.0, 0.0, 1.0, 0.0, 0.0),
+		mat3( 0.0, 0.0, 1.0, 0.0,-1.0, 0.0,-1.0, 0.0, 0.0),
+		mat3( 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0),
+		mat3( 1.0, 0.0, 0.0, 0.0, 0.0,-1.0, 0.0,-1.0, 0.0),
+		mat3( 1.0, 0.0, 0.0, 0.0,-1.0, 0.0, 0.0, 0.0, 1.0),
+		mat3(-1.0, 0.0, 0.0, 0.0,-1.0, 0.0, 0.0, 0.0,-1.0))[f];
+}
+
+vec3 getCubeCoord() {
+	mat3 cubeFace = getCubeFace(faceIdx);
+	return cubeFace[0]*vertCoord.x + cubeFace[1]*vertCoord.y + cubeFace[2];
+}
+
+vec3 getSampleCoord(int f, int x, int y) {
+	float ics = 1.0 / float(cubeSide-1);
+	mat3 face = getCubeFace(f);
+	vec2 sampleCoord = vec2(
+		mix(-1.0, 1.0, float(x)*ics),
+		mix(-1.0, 1.0, float(y)*ics));
+	return face[0]*sampleCoord.x + face[1]*sampleCoord.y + face[2];
+}
+
 void main() {
-  mat3 cubeFace = cubeFaces[faceIdx];
-  vec3 cubeCoord =
-    cubeFace[0]*vertCoord.x+
-    cubeFace[1]*vertCoord.y+
-    cubeFace[2];
-  float ics = 1.0 / float(cubeSide-1);
-  vec4 accumColor = vec4(0.0);
-  float accumWeight = 0.0;
-  for(int f = 0; f < 6; ++f) {
-    mat3 sampleFace = cubeFaces[f];
-    for(int y = 0; y < cubeSide; ++y) {
-      for(int x = 0; x < cubeSide; ++x) {
-        vec2 sampleCoord = vec2(
-          mix(-1.0, 1.0, float(x)*ics),
-          mix(-1.0, 1.0, float(y)*ics));
-        vec3 sampleCubeCoord =
-          sampleFace[0]*sampleCoord.x+
-          sampleFace[1]*sampleCoord.y+
-          sampleFace[2];
-        vec4 sampleColor = texture(cubeMap, sampleCubeCoord);
-        float sampleWeight = pow(max(dot(
-            normalize(cubeCoord),
-            normalize(sampleCubeCoord)),
-          0.0), pow(2.0, float(sharpness)));
-        accumColor = accumColor + sampleColor * sampleWeight;
-        accumWeight += sampleWeight;
-      }
-    }
-  }
-  fragColor = accumColor / accumWeight;
+	vec3 cubeCoord = getCubeCoord();
+	vec4 accumColor = vec4(0.0);
+	float accumWeight = 0.0;
+	for(int f = 0; f < 6; ++f) {
+		for(int y = 0; y < cubeSide; ++y) {
+			for(int x = 0; x < cubeSide; ++x) {
+				vec3 sampleCubeCoord = getSampleCoord(f, x, y);
+				vec4 sampleColor = texture(cubeMap, sampleCubeCoord);
+				float sampleWeight = pow(max(dot(
+						normalize(cubeCoord),
+						normalize(sampleCubeCoord)),
+					0.0), pow(2.0, float(sharpness)));
+				accumColor = accumColor + sampleColor * sampleWeight;
+				accumWeight += sampleWeight;
+			}
+		}
+	}
+	fragColor = accumColor / accumWeight;
 }
 /* -------------------------------------------------------------------------- */
 	)"};
@@ -154,6 +160,8 @@ auto eagitexi_cubemap_blur_renderer::_build_program(
     const auto fs{gl.create_shader.object(GL.fragment_shader)};
     gl.shader_source(fs, oglplus::glsl_string_ref(_fs_source()));
     gl.compile_shader(fs);
+    gl_api().shader_info_log(fs).and_then(
+      [](const auto& s) { std::cout << s << std::endl; });
 
     // program
     auto prog{gl.create_program.object()};
