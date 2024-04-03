520bdd5fadac7d774a0d42a5a84ab0c9f87c62fa
Matus Chochlik <chochlik@gmail.com>
2024-04-03 07:10:09 +0200

    Added headers to some GLSL sources

--- source/app/model_viewer/glsl/edges_wrap_frag.glsl
+++ source/app/model_viewer/glsl/edges_wrap_frag.glsl
@@ -1,3 +1,8 @@
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+/// https://www.boost.org/LICENSE_1_0.txt
+///
 #version 140
 
 const float EdgeWidth = 0.8;
--- source/app/resource_provider/glsl/edges_default_frag.glsl
+++ source/app/resource_provider/glsl/edges_default_frag.glsl
@@ -1,4 +1,9 @@
-#version 140
+#/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+/// https://www.boost.org/LICENSE_1_0.txt
+///
+version 140
 
 const float EdgeWidth = 0.8;
 
