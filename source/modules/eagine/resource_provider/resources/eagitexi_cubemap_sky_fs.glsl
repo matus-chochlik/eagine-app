95547e38aa7f181b57e64facca65549dbb8e5536
Matus Chochlik <chochlik@gmail.com>
2024-05-03 12:22:08 +0200

    Tweaks in eagitexi_cubemap_sky_renderer

--- source/modules/eagine/resource_provider/resources/eagitexi_cubemap_sky_fs.glsl
+++ source/modules/eagine/resource_provider/resources/eagitexi_cubemap_sky_fs.glsl
@@ -291,6 +291,7 @@ struct AtmosphereSample {
 	float hitLight;
 	float directLight;
 	float sunUp;
+	float altitude;
 	float atmDensity;
 	float atmDistance;
 	float atmDistRatio;
@@ -318,6 +319,7 @@ AtmosphereSample atmSample(
 
 	float planetHit = sign(max(hitNear(sphereHit(lightRayS, planet)), 0.0));
 	float planetShadow = 1.0 - planetHit;
+	float altitude = distance(lightRay0.origin, planet.center) - planet.radius;
 
 	return AtmosphereSample(
 		viewRay,
@@ -326,6 +328,7 @@ AtmosphereSample atmSample(
 		max(sign(lightAngle-acos(directLight)), 0.0),
 		directLight,
 		sqrt(to01(viewRay.direction.y)),
+		clamp(altitude / planetRadius, 0.0, 1.0),
 		pow(clamp(
 			distance(lightRay0.origin, planet.center) - planet.radius /
 			atmosphere.radius - planet.radius,
@@ -479,7 +482,9 @@ vec4 clearAirColor(AtmosphereSample a, AtmosphereShadow s) {
 		airColor.rgb,
 		mix(airColor.a * shadow, 1.0, a.hitLight * shadow));
 
-	airColor += vec4(0.25, 0.40, 0.70, 0.25) * a.sunUp * (1.0 - shadow);
+
+	airColor += vec4(0.20, 0.35, 0.60, 0.25) *
+		 (1.0 - shadow) * pow(a.sunUp * a.altitude, 0.25);
 
 	return airColor;
 }
