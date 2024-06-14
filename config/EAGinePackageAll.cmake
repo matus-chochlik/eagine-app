1f7f4b3601feb7c68fe3b03942bf79cec703f681
Matus Chochlik <chochlik@gmail.com>
2024-06-14 22:54:35 +0200

    Changed how packages are built

---
+++ config/EAGinePackageAll.cmake
@@ -0,0 +1,12 @@
+#  Copyright Matus Chochlik.
+#  Distributed under the Boost Software License, Version 1.0.
+#  See accompanying file LICENSE_1_0.txt or copy at
+#  https://www.boost.org/LICENSE_1_0.txt
+#
+# Package specific options
+#  Debian
+#   Dependencies
+set(CPACK_DEBIAN_ALL-TOOLS_PACKAGE_DEPENDS "python3,python3-pip")
+#   Descriptions
+set(CPACK_DEBIAN_ALL-TOOLS_DESCRIPTION "Collection of command-line utilities useful with EAGine applications.")
+
