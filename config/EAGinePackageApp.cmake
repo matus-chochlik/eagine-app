98dd18dba3c90f33b4e6713b3d48cd774385bd53
Matus Chochlik <chochlik@gmail.com>
2024-06-16 12:09:46 +0200

    Added the examples install component + updated sub-modules

--- config/EAGinePackageApp.cmake
+++ config/EAGinePackageApp.cmake
@@ -8,12 +8,14 @@
 #   Dependencies
 set(CXX_RUNTIME_PKGS "libc6,libc++1-17")
 set(CPACK_DEBIAN_APP-TOOLS_PACKAGE_DEPENDS "python3,python3-pip")
-set(CPACK_DEBIAN_APP-MODEL-VIEWER_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,libzip4,libglfw3,libglew2.2,libopenal1,libalut0,libc++1-17")
-set(CPACK_DEBIAN_APP-SKY-VIEWER_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,libzip4,libglfw3,libglew2.2,libopenal1,libalut0,libc++1-17")
-set(CPACK_DEBIAN_APP-TILING-VIEWER_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,libzip4,libglfw3,libglew2.2,libopenal1,libalut0,libc++1-17")
-set(CPACK_DEBIAN_APP-RESOURCE-PROVIDER_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,libzip4,libglfw3,libglew2.2,libopenal1,libalut0,libc++1-17")
+set(CPACK_DEBIAN_APP-EXAMPLES_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g,libssl3,libzip4,libglfw3,libglew2.2,libopenal1,libalut0")
+set(CPACK_DEBIAN_APP-MODEL-VIEWER_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g,libssl3,libzip4,libglfw3,libglew2.2,libopenal1,libalut0")
+set(CPACK_DEBIAN_APP-SKY-VIEWER_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g,libssl3,libzip4,libglfw3,libglew2.2,libopenal1,libalut0")
+set(CPACK_DEBIAN_APP-TILING-VIEWER_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g,libssl3,libzip4,libglfw3,libglew2.2,libopenal1,libalut0")
+set(CPACK_DEBIAN_APP-RESOURCE-PROVIDER_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g,libssl3,libzip4,libglfw3,libglew2.2,libopenal1,libalut0")
 #   Descriptions
 set(CPACK_DEBIAN_APP-TOOLS_DESCRIPTION "Collection of command-line utilities useful with EAGine applications.")
+set(CPACK_DEBIAN_APP-TOOLS_DESCRIPTION "EAGine App examples.")
 set(CPACK_DEBIAN_APP-SKY-VIEWER_DESCRIPTION "Application for viewing sky-box textures.")
 set(CPACK_DEBIAN_APP-MODEL-VIEWER_DESCRIPTION "Application for viewing 3D models.")
 set(CPACK_DEBIAN_APP-TILING-VIEWER_DESCRIPTION "Application for viewing tiling and tileset textures.")
