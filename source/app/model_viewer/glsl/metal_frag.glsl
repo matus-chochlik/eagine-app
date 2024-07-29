56aecdcab5730ee6451e4b8a85b38c2cc9080331
Matus Chochlik <chochlik@gmail.com>
2024-07-29 19:18:06 +0200

    Small changes in model viewer Metal shader

--- source/app/model_viewer/glsl/metal_frag.glsl
+++ source/app/model_viewer/glsl/metal_frag.glsl
@@ -15,16 +15,18 @@ out vec3 fragColor;
 uniform samplerCube SkyBox;
 
 void main() {
+	float pow2Rough = pow(vertRough, 2.0);
+	float sqrtRough = sqrt(vertRough);
     vec3 ReflCoord = reflect(normalize(vertViewDir), normalize(vertNormal));
-    vec3 CubeCoord = normalize(mix(ReflCoord, vertNormal, pow(vertRough, 2.0)));
+    vec3 CubeCoord = normalize(mix(ReflCoord, vertNormal, pow2Rough));
 
-    float lod = mix(0.0, 7.0, sqrt(vertRough));
+    float lod = mix(0.0, 7.0, pow2Rough);
     vec3 Reflect = mix(
-        textureLod(SkyBox, CubeCoord, int(floor(lod))).rgb,
-        textureLod(SkyBox, CubeCoord, int(ceil(lod))).rgb,
-        fract(lod));
+        textureLod(SkyBox, CubeCoord, int(floor(lod))),
+        textureLod(SkyBox, CubeCoord, int(ceil(lod))),
+        fract(lod)).rgb;
     vec3 Diffuse = vec3(max(dot(vertNormal, vertLightDir)+0.1, 0.0) * 0.4);
-    vec3 Ambient = vec3(0.2) + Reflect * mix(1.0, 0.0, vertOccl);
+    vec3 Ambient = mix(Reflect * mix(1.0, 0.0, vertOccl), vec3(0.2), sqrtRough);
 
-    fragColor = vertColor * (Ambient + Diffuse) + Reflect * vertOccl;
+    fragColor = vertColor * (Ambient * vertOccl + Diffuse) + Reflect * vertOccl;
 }
