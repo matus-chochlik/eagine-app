252f8a4e7d6e117d872347f3d88fca2cb83480b0
Matus Chochlik <chochlik@gmail.com>
2024-08-08 06:44:06 +0200

    Changes in the model viewer metal shader

--- source/app/model_viewer/glsl/metal_frag.glsl
+++ source/app/model_viewer/glsl/metal_frag.glsl
@@ -13,6 +13,7 @@ in float vertOccl;
 out vec3 fragColor;
 
 uniform samplerCube SkyBox;
+const float maxLod = 7.0;
 
 void main() {
 	float pow2Rough = pow(vertRough, 2.0);
@@ -20,13 +21,21 @@ void main() {
     vec3 ReflCoord = reflect(normalize(vertViewDir), normalize(vertNormal));
     vec3 CubeCoord = normalize(mix(ReflCoord, vertNormal, pow2Rough));
 
-    float lod = mix(0.0, 7.0, pow2Rough);
-    vec3 Reflect = mix(
-        textureLod(SkyBox, CubeCoord, int(floor(lod))),
-        textureLod(SkyBox, CubeCoord, int(ceil(lod))),
-        fract(lod)).rgb;
-    vec3 Diffuse = vec3(max(dot(vertNormal, vertLightDir)+0.1, 0.0) * 0.4);
-    vec3 Ambient = mix(Reflect * mix(1.0, 0.0, vertOccl), vec3(0.2), sqrtRough);
+    float rlod = mix(0.0, maxLod, pow2Rough);
+    vec4 Reflect = mix(
+        textureLod(SkyBox, CubeCoord, int(floor(rlod))),
+        textureLod(SkyBox, CubeCoord, int(ceil(rlod))),
+        fract(rlod));
+    float alod = mix(maxLod-1.0, maxLod, sqrtRough);
+    vec4 Ambient = mix(
+        textureLod(SkyBox, CubeCoord, int(floor(alod))),
+        textureLod(SkyBox, vertNormal, int(ceil(alod))),
+        fract(alod));
 
-    fragColor = vertColor * (Ambient * vertOccl + Diffuse) + Reflect * vertOccl;
+    vec3 Diffuse = vec3(max(dot(vertNormal, vertLightDir)+0.1, 0.0) * 0.4);
+
+	fragColor =
+		(Ambient.rgb * min(Ambient.a + 0.2, 1.0) + Diffuse) * vertOccl * vertColor +
+		(Reflect.rgb * min(Reflect.a + 0.1, 1.0)) * vertOccl;
+
 }
